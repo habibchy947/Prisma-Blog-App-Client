7b4cf088687a6ef46684832c0d475ac3399c716d
habibchy947 <habibullahalquaderi2005@gmail.com>
2026-01-23 11:51:27 -0600

    add login and signup page

--- src/components/layout/navbar.tsx
+++ src/components/layout/navbar.tsx
@@ -78,7 +78,7 @@ const Navbar = ({
   ],
   auth = {
     login: { title: "Login", url: "/login" },
-    signup: { title: "Sign up", url: "/register" },
+    signup: { title: "Sign up", url: "/signup" },
   },
   className,
 }: Navbar1Props) => {
