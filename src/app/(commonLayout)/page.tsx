d36f48341d46f65c54898158f992747704304fec
habibchy947 <habibullahalquaderi2005@gmail.com>
2026-01-25 03:01:26 -0600

    implement role based dashboard

--- src/app/(commonLayout)/page.tsx
+++ src/app/(commonLayout)/page.tsx
@@ -1,8 +1,8 @@
 import { Button } from "@/components/ui/button";
-import { userService } from "@/services/user.service";
+import { blogService } from "@/services/blog.service";
 
 export default async function Home() {
-  const {data} = await userService.getSession();
+  const {data} = await blogService.getBlogPosts();
   console.log(data);
 
   return (
