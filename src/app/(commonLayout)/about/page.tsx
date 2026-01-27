acbc2d51220a6cb8323980fe73bd306f9772f866
habibchy947 <habibullahalquaderi2005@gmail.com>
2026-01-27 06:18:43 -0600

    create login and registration form and its implementation

--- src/app/(commonLayout)/about/page.tsx
+++ src/app/(commonLayout)/about/page.tsx
@@ -1,13 +1,29 @@
+"use client";
 
-export const dynamic = "force-dynamic";
+import { getBlogs } from "@/actions/blog.action";
+import { useEffect, useState } from "react";
 
-const AboutPage = async () => {
+// export const dynamic = "force-dynamic";
+
+const AboutPage = () => {
     // For simulating loading time
-    await new Promise((resolve) => setTimeout(resolve, 4000));
+    // await new Promise((resolve) => setTimeout(resolve, 4000));
 
     // For simulating error
     // throw new Error("Something went wrong");
-    
+    const [data, setData] = useState();
+    const [error, setError] = useState<{message: string} | null>(null);
+    console.log(data);
+    console.log(error);
+
+    useEffect(() => {
+        (async () => {
+            const { data, error } = await getBlogs();
+            setData(data);
+            setError(error)
+        })();
+    }, []);
+
     return (
         <div>
             <h1>This is about page.</h1>
